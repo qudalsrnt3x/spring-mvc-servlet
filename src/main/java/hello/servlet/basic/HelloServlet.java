19092fb4aa3de090ca57cca62497cedc6bca5f04
qudals <qudalsrnt3x@gmail.com>
2021-03-15 22:54:29 +0900

    Update HelloServlet - 서블릿을 쓰면 http스펙을 쉽게 이용할 수 있다.

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -19,5 +19,19 @@ public class HelloServlet extends HttpServlet {
 
         // request, response 객체가 생성된다.
         // WAS 서버들이 서블릿 표준 스펙을 구현한다.
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        // 쿼리파라미터 출력 가능
+
+        String pwd = request.getParameter("pwd");
+        System.out.println("pwd = " + pwd);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8"); // 두 줄은 contentType 에 들어간다.
+        response.getWriter().write("hello " + username);
+
+        // 우리가 http 스펙을 직접 개발해서 쓰려면 엄청 복작하다.
+        // 하지만 서블릿을 이용해서 자주쓰는 기능들을 쉽게 쓸 수 있다.
     }
 }
